349977b6278f19a8fd35c811b725321282cb5040
scandal0us <d.l.lutsak@gmail.com>
2019-11-25 23:45:48 +0200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import { Provider } from 'react-redux';
+import './App.scss';
+import { store } from './store';
+import { RestaurantsListPage } from './components/RestaurantsListPage';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
-const App = () => (
-  <div className="App">
-    <h1>React Uber eats</h1>
-  </div>
+export const App = () => (
+  <Provider store={store}>
+    <Header />
+    <div className="page">
+      <main className="content">
+        <RestaurantsListPage />
+      </main>
+    </div>
+    <Footer />
+  </Provider>
 );
-
-export default App;
---
+++ src/components/Test/Test.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Test extends Component {
+  render() {
+    return (
+      <div className="content">
+          
+      </div>
+    );
+  }
+}
+
+Test.prototypes = {
+
+};
